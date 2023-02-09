e767f0ee9e3dc45e96e94ef3fc263e98d17784e6
mdumitrescu27 <114684484+mdumitrescu27@users.noreply.github.com>
2023-02-09 12:52:27 +0200

    .

--- insta.js
+++ insta.js
@@ -23,14 +23,17 @@ for (let i = 0; i < profileImg.length; i++) {
 const storyClick = document.querySelectorAll(".checkbox");
 const storyView = document.querySelector(".open-story");
 const storyProfileImg = document.querySelector(".story-profile-img");
+const bodyContainer = document.getElementsByClassName("body-container");
 
 function openStory() {
   for (let i = 0; i < storyClick.length; i++) {
     if (storyClick[i].checked) {
       storyProfileImg.src = profileImg[i].src;
       storyView.style.visibility = "visible";
+      document.body.classList.add("stop-scrolling");
     }
   }
+  playNext();
 }
 
 for (let i = 0; i < storyClick.length; i++) {
@@ -42,9 +45,51 @@ const closeStoryBtn = document.querySelector(".close-story-button");
 
 function closeStory() {
   storyView.style.visibility = "hidden";
+  document.body.classList.remove("stop-scrolling");
   storyClick.forEach((element) => {
     element.checked = false;
   });
 }
 
 closeStoryBtn.addEventListener("click", closeStory);
+
+// PROGRESS-BAR TEST
+const progressContainer = document.querySelector(".progress-container");
+const progress = Array.from(document.querySelectorAll(".progress"));
+const status = document.querySelector(".status");
+
+function playNext(e) {
+  const current = e && e.target;
+  let next;
+  console.log(progress[0]);
+
+  if (current) {
+    const currentIndex = progress.indexOf(current);
+    if (currentIndex < progress.length) {
+      next = progress[currentIndex + 1];
+    }
+    // current.classList.remove("active");
+    // current.classList.add("passed");
+  }
+
+  if (!next) {
+    progress.map((el) => {
+      // el.classList.remove("active");
+      // el.classList.remove("passed");
+    });
+    next = progress[0];
+  }
+  next.classList.add("active");
+}
+
+const clickHandler = (e) => {
+  const index = Math.floor(
+    e.offsetX / (progressContainer.clientWidth / progress.length)
+  );
+  status.innerText = "Clicked " + index;
+};
+
+progress.map((el) => el.addEventListener("animationend", playNext, false));
+progressContainer.addEventListener("click", clickHandler, false);
+
+console.log(document.body.style.overflow);
